004c55c2b479bd38c347399e2b2329cc83d2e87f
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-09-02 14:16:14 +0100

    added .env support

--- check-ip.sh
+++ check-ip.sh
@@ -2,17 +2,41 @@
 
 set -e
 
-IP4_ADD_CURRENT=$(curl -4 icanhazip.com 2>/dev/null)
-IP6_ADD_CURRENT=$(curl -6 icanhazip.com 2>/dev/null)
-IP4_ADD_DNS=$(dig @ns1.1stcall.uk +short rpicm4-1.1stcall.uk A)
-IP6_ADD_DNS=$(dig @ns1.1stcall.uk +short rpicm4-1.1stcall.uk AAAA)
+declare IP4_ADD_CURRENT
+declare IP6_ADD_CURRENT
+declare IP4_ADD_DNS
+declare IP6_ADD_DNS
 
+declare IP_LOOKUP_ADD
+declare DNS_PROTOCOL
+declare DNS_SERVER
+declare DOMAIN
+declare HOST
+
+[ -f .env ] && source .env
+
+IP_LOOKUP_ADD=${IP_LOOKUP_ADD:-icanhazip.com}
+DNS_PROTOCOL=${DNS_PROTOCOL:-http}
+DNS_SERVER=${DNS_SERVER:-localhost:5380}
+DOMAIN=${DOMAIN:-1stcall.uk}
+HOST=${HOST:-rpicm4-1}
+
+IP4_ADD_CURRENT=$(curl -4 ${IP_LOOKUP_ADD} 2>/dev/null)
+IP6_ADD_CURRENT=$(curl -6 ${IP_LOOKUP_ADD} 2>/dev/null)
+IP4_ADD_DNS=$(dig @${DNS_SERVER} +short ${HOST}.${DOMAIN} A)
+IP6_ADD_DNS=$(dig @${DNS_SERVER} +short ${HOST}.${DOMAIN} AAAA)
+
+printf "IP lookup address is \'%s\'\n" $IP_LOOKUP_ADD
+printf "DNS API protocol is \'%s\'\n" $DNS_PROTOCOL
+printf "DNS API server is \'%s\'\n" $DNS_SERVER
+printf "Domain to check is \'%s\'\n" $DOMAIN
+printf "Hostname to update is \'%s\'\n" $HOST
 printf "Current adresses are IP4 %s and IP6 %s\n" $IP4_ADD_CURRENT $IP6_ADD_CURRENT
 printf "DNS reports adresses are IP4 %s and IP6 %s\n" $IP4_ADD_DNS $IP6_ADD_DNS
 
 if [ $IP4_ADD_CURRENT != $IP4_ADD_DNS ]; then
-    TOKEN=$(curl https://ns1.1stcall.uk/api/user/login?user=carl\&pass=Manager09\&includeInfo=false 2>/dev/null | jq -r '.token')
-    responce=$(curl https://ns1.1stcall.uk/api/zones/records/update?token=${TOKEN}\&domain=rpicm4-1.1stcall.uk\&zone=1stcall.uk\&type=A\&value=$IP4_ADD_DNS\&newValue=$IP4_ADD_CURRENT\&ptr=false 2>/dev/null)
+    TOKEN=$(curl ${DNS_PROTOCOL}://${DNS_SERVER}/api/user/login?user=carl\&pass=Manager09\&includeInfo=false 2>/dev/null | jq -r '.token')
+    responce=$(curl ${DNS_PROTOCOL}://${DNS_SERVER}/api/zones/records/update?token=${TOKEN}\&domain=${HOST}.${DOMAIN}\&zone=${DOMAIN}\&type=A\&value=$IP4_ADD_DNS\&newValue=$IP4_ADD_CURRENT\&ptr=false 2>/dev/null)
     status=$(echo $responce | jq -r '.status')
 
     if [ $status == "ok" ]; then
@@ -27,8 +51,8 @@ else
 fi
 
 if [ $IP6_ADD_CURRENT != $IP6_ADD_DNS ]; then
-    TOKEN=${TOKEN:-$(curl https://ns1.1stcall.uk/api/user/login?user=carl\&pass=Manager09\&includeInfo=false 2>/dev/null | jq -r '.token')}
-    responce=$(curl https://ns1.1stcall.uk/api/zones/records/update?token=${TOKEN}\&domain=rpicm4-1.1stcall.uk\&zone=1stcall.uk\&type=AAAA\&value=$IP6_ADD_DNS\&newValue=$IP6_ADD_CURRENT\&ptr=true 2>/dev/null)
+    TOKEN=${TOKEN:-$(curl ${DNS_PROTOCOL}://${DNS_SERVER}/api/user/login?user=carl\&pass=Manager09\&includeInfo=false 2>/dev/null | jq -r '.token')}
+    responce=$(curl ${DNS_PROTOCOL}://${DNS_SERVER}/api/zones/records/update?token=${TOKEN}\&domain=${HOST}.${DOMAIN}\&zone=${DOMAIN}\&type=AAAA\&value=$IP6_ADD_DNS\&newValue=$IP6_ADD_CURRENT\&ptr=true 2>/dev/null)
     status=$(echo $responce | jq -r '.status')
 
     if [ $status == "ok" ]; then
@@ -43,7 +67,7 @@ else
 fi
 
 if [ ${TOKEN:-unset} != unset ]; then
-    responce=$(curl https://ns1.1stcall.uk/api/user/logout?token=$TOKEN 2>/dev/null)
+    responce=$(curl ${DNS_PROTOCOL}://${DNS_SERVER}/api/user/logout?token=$TOKEN 2>/dev/null)
     status=$(echo $responce | jq -r '.status')
     if [ $status == ok ]; then
         printf "Logout successfull\n"
